8b0daadeddfa591c2006041b8aa4498d2f36ed25
VergaraC <victorvaac@al.insper.edu.br>
2021-03-30 23:53:04 -0300

    Inicio

--- dna.c
+++ dna.c
@@ -18,11 +18,24 @@ void load_string(FILE *file, int *p, char *s, int t) {
 
 
 int mlcs_w(char a[], int n, char b[], int m, int length[MAX_SIZE + 1][MAX_SIZE + 1]) {
+    if(n == 0 || m == 0){
+        length[n][m] = 0;
+    }else if(a[n-1] == b[m-1]){
+        length[n][m] = length[n-1][m-1];
+    }else if(length[n-1][m] > length[n][m-1]){
+        length[n][m] = length[n-1][m];
+    }else if(length[n][m-1] > length[n-1][m]){
+        length[n][m] = length[n][m-1];
+    }
+    mlcs_w (a,n-1,b,m-1,length);
     return 0;
 }
 
 
 int mlcs(char a[], int n, char b[], int m) {
+    int length[n][m];
+    printf("Chamando");
+    mlcs_w(a,n,b,m,length);
     return 1;
 }
 
