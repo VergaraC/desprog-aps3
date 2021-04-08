c49742bde086493e0ff03925ced5a088abaf54b0
VergaraC <victorvaac@al.insper.edu.br>
2021-04-07 21:41:24 -0300

    N roda o Teste de Integration, Fica no loop infinito

--- dna.c
+++ dna.c
@@ -49,5 +49,31 @@ int mlcs(char a[], int n, char b[], int m) {
 
 
 int dlcs(char a[], int n, char b[], int m) {
-    return 2;
+
+    int length[n][m+1];
+
+    for(int i=0; i<n; i++){
+
+        for(int j=0; j<m; j++){
+
+            if(i==0 || j==0){
+                length[i][j] = 0;
+            }
+
+            else if(a[i-1] == b[j-1]){
+                length[i][j] = length[i-1][j-1]+1;
+            }
+
+            else{
+                if(length[i-1][j] >  length[i][j-1]){
+                   length[i][j] = length[i-1][j];
+                }else{
+                    length[i][j] = length[i][j-1];
+                }
+            }   
+        }
+    }
+
+    return length[n][m+1];
 }
+
