0d983c58e378f1661db4d7b8bea4a05ac9a04e89
VergaraC <victorvaac@al.insper.edu.br>
2021-04-08 15:28:17 -0300

    Caveira Capitao

--- dna.c
+++ dna.c
@@ -19,29 +19,30 @@ void load_string(FILE *file, int *p, char *s, int t) {
 
 int mlcs_w(char a[], int n, char b[], int m, int length[MAX_SIZE + 1][MAX_SIZE + 1]) {
     if(n == 0 || m == 0){
-        return 0;
+        length[n][m] = 0;
+        return length[n][m];
+    }else if(length[n][m] != -1){
+        return length[n][m];
     }else if(a[n-1] == b[m-1]){
-        return 1 +  mlcs_w(a,n-1,b,m-1,length);
+        length[n][m] =  1 +  mlcs_w(a,n-1,b,m-1,length);
+        return length[n][m];
     }else {
         int n1 = mlcs_w(a,n-1,b,m,length);
         int m1 = mlcs_w(a,n,b,m-1,length);
         if(n1 > m1){
-            return n1;
+            length[n][m] = n1;
+            return length[n][m];
         } 
-        return m1;
+        length[n][m] = m1;
+        return length[n][m];
     }
 }
 int mlcs(char a[], int n, char b[], int m) {
     int length[MAX_SIZE + 1][MAX_SIZE + 1];
     
-    for (int i = 0; i < m; i++){
-        for (int j = 0; j < n; j++){
-            length[i][j] = a[j]; 
-        }
-    }
-    for (int i2 = 0; i2 < n; i2++){
-        for (int j2 = 0; j2 < m; j2++){
-            length[i2][j2] = b[j2]; 
+    for (int i = 0; i <= n; i++){
+        for (int j = 0; j <= m; j++){
+            length[i][j] = -1; 
         }
     }
     return mlcs_w(a,n,b,m,length);
@@ -50,7 +51,9 @@ int mlcs(char a[], int n, char b[], int m) {
 
 int dlcs(char a[], int n, char b[], int m) {
 
-    int length[n + 1][m + 1];
+    int length[MAX_SIZE + 1][MAX_SIZE + 1];
+    int max = 0;
+    int left, right;
 
     for(int i = 0; i <= n; i++){
 
@@ -59,16 +62,20 @@ int dlcs(char a[], int n, char b[], int m) {
             if(i == 0 || j == 0){
                 length[i][j] = 0;
             }else if(a[i-1] == b[j-1]){
-                length[i][j] = length[i-1][j-1]+1;
+                length[i][j] = length[i-1][j-1] + 1;
             }else{
-                if(length[i-1][j] >  length[i][j-1]){
-                   length[i][j] = length[i-1][j];
-                }else{
-                    length[i][j] = length[i][j-1];
-                }
-            }   
+                left = length[i-1][j];
+                right = length[i][j-1];
+                length[i][j] = right;
+                if(left > right){
+                   length[i][j] = left;
+                } 
+            }  
+            if(length[i][j]> max){
+            max = length[i][j];
+            }
         }
     }
-    return length[n][m];
+    return max;
 }
 
